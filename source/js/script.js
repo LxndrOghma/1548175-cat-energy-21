f9494a2c76c2bf673ea4ec341ccff934bed29c38
LxndrOghma <Brikman424@gmail.com>
2021-01-13 21:58:01 +0300

    Внёс правки для получения допуска

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,20 @@
-let navMain = document.querySelector('.main-nav');
-let headerToggle = document.querySelector('.page-header__toggle');
-let headerNav = document.querySelector('.page-header__logo-wrapper');
+let navMain = document.querySelector(".main-nav");
+let headerToggle = document.querySelector(".page-header__toggle");
+let headerNav = document.querySelector(".page-header__logo-wrapper");
 
-navMain.classList.remove('main-nav--nojs');
+headerToggle.classList.remove("visually-hidden");
+navMain.classList.remove("main-nav--nojs");
 
-headerToggle.addEventListener('click', function() {
-  if (headerNav.classList.contains('page-header__logo-wrapper--nav-closed')) {
-    navMain.classList.remove('main-nav--closed');
-    headerNav.classList.remove('page-header__logo-wrapper--nav-closed');
-    navMain.classList.add('main-nav--opened');
-    headerNav.classList.add('page-header__logo-wrapper--nav-opened');
+headerToggle.addEventListener("click", function() {
+  if (headerNav.classList.contains("page-header__logo-wrapper--nav-closed")) {
+    navMain.classList.remove("main-nav--closed");
+    headerNav.classList.remove("page-header__logo-wrapper--nav-closed");
+    navMain.classList.add("main-nav--opened");
+    headerNav.classList.add("page-header__logo-wrapper--nav-opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    headerNav.classList.add('page-header__logo-wrapper--nav-closed');
-    navMain.classList.remove('main-nav--opened');
-    headerNav.classList.remove('page-header__logo-wrapper--nav-opened');
+    navMain.classList.add("main-nav--closed");
+    headerNav.classList.add("page-header__logo-wrapper--nav-closed");
+    navMain.classList.remove("main-nav--opened");
+    headerNav.classList.remove("page-header__logo-wrapper--nav-opened");
   }
 });
